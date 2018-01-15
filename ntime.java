108e49fe81172ec57da6763719ab03ea7fd4ce1d
Akil <34641197+akil007@users.noreply.github.com>
2018-01-15 22:01:30 +0530

    Create ntime.java

---
+++ ntime.java
@@ -0,0 +1,24 @@
+package positive.negative;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class PositiveNegative {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int a= scan.nextInt();
+       
+            for(int i=0;i<a;i++)
+            {
+                System.out.println("HELLO");
+            }
+        
+    }
+    
+}

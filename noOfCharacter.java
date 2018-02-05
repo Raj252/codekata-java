9c17aaee932b618055bff7cda836ae687accdd3a
Akil <34641197+akil007@users.noreply.github.com>
2018-02-05 06:21:46 +0530

    Create noOfCharacter.java

---
+++ noOfCharacter.java
@@ -0,0 +1,32 @@
+package sieves;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class Sieves {
+
+    
+   
+    public static void main(String a[]){
+       Scanner scan= new Scanner(System.in);
+       String str= scan.nextLine();
+       str=str.trim();
+       int n= str.length();
+       int count=0;
+       char[] ch = str.toCharArray();
+       for(int i=0;i<n;i++)
+       {
+           
+           
+           if(ch[i]==' ')
+           {
+               count++;
+           }
+       }
+       System.out.println(n-count);
+        
+        
+ 
+    }
+}

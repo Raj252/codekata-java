5284ace0d62c52918f5352103e8f74e13d403c44
Akil <34641197+akil007@users.noreply.github.com>
2018-02-08 09:42:30 +0530

    Create nextNo.java

---
+++ nextNo.java
@@ -0,0 +1,19 @@
+package special;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class Special {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        
+        System.out.println(n+1);
+}
+}

cc5dfebf02d1b7126c64abd36c2117941244fea0
Akil <34641197+akil007@users.noreply.github.com>
2018-02-08 08:07:31 +0530

    Create noOfstrings.java

---
+++ noOfstrings.java
@@ -0,0 +1,28 @@
+package special;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class Special {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan=new Scanner(System.in);
+        String name=scan.next();
+        int n= scan.nextInt();
+        
+        
+        
+      for(int i=0;i<n;i++)
+      {
+          
+          
+          System.out.printf("%s ",name);
+          
+      }
+}
+}

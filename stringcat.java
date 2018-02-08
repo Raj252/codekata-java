329838de0fd137cac1ed5b731c428121b184fa1b
Akil <34641197+akil007@users.noreply.github.com>
2018-02-08 09:31:31 +0530

    Create stringcat.java

---
+++ stringcat.java
@@ -0,0 +1,24 @@
+package special;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class Special {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan=new Scanner(System.in);
+        int cnt1=0,cnt2=0;
+        String name=scan.next();
+       
+        String name2= scan.next();
+        String name3;
+        name3=name+name2;
+        System.out.print(name3);
+                
+}
+}

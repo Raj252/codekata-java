29c36e3b4c03b25ebed1090d55ff0e10891be6d5
Akil <34641197+akil007@users.noreply.github.com>
2018-02-08 09:59:46 +0530

    Create sumoftwono.java

---
+++ sumoftwono.java
@@ -0,0 +1,21 @@
+package special;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class Special {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int m=scan.nextInt();
+        int sum = n+m;
+        System.out.println(sum);
+      
+}
+}

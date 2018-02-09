d0937a75be96c57229e8c0cffc8f5c4ce59511c8
Akil <34641197+akil007@users.noreply.github.com>
2018-02-09 22:57:16 +0530

    Create multiplyToCheckEVEN.java

---
+++ multiplyToCheckEVEN.java
@@ -0,0 +1,31 @@
+package sieves;
+import java.util.*;
+/**
+ *
+ * @author akil
+ */
+public class Sieves {
+
+    
+   
+    public static void main(String a[]){
+       
+       Scanner scan= new Scanner(System.in);
+       int n=scan.nextInt();
+       int m=scan.nextInt();
+       n*=m;
+       if(n%2==0)
+       {
+          System.out.println("YES");
+    
+       }
+       else
+       {
+        
+         System.out.println("NO");
+    }
+       }
+       
+}
+
+

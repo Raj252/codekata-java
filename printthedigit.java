6ae7b5afcc93a3151e5b30ae1bf25e4b3b26473a
Akil <34641197+akil007@users.noreply.github.com>
2018-02-09 22:38:37 +0530

    Update printthedigit.java

--- printthedigit.java
+++ printthedigit.java
@@ -1,34 +1,26 @@
-package sieves;
+package special;
 import java.util.*;
 /**
  *
  * @author akil
  */
-public class Sieves {
+public class Special {
 
-    
-   
-    public static void main(String a[]){
-       
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
        Scanner scan= new Scanner(System.in);
-       int n=scan.nextInt();
-       int[] j=new int[100];
-       int i=0,r=n%10,count=0;
-       while(r!=0)
+       String str=scan.next();
+       int len= str.length();
+       char[] ch=str.toCharArray();
+       for(int i=0;i<len;i++)
        {
-       r=n%10;
-       n=n/10;
-       
-       j[i]=r;
-       i++;
-       count++;
-       
-       }
-       for( i=count-2;i>=0;i--)
-       {
-           System.out.printf("%d ",j[i]);
+           System.out.printf("%s ",ch[i]);
        }
        
-    }
+        
+}
 }
 
